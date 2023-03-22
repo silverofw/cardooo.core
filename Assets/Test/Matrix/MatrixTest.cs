8102cdb9c921eabb344b3a30e88e9fe5e246ff34
Silver <silverofw@gmail.com>
2023-03-23 02:59:39 +0800

    [UPDATE] matrix math

---
+++ Assets/Test/Matrix/MatrixTest.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using cardooo.math;
+
+public class MatrixTest : MonoBehaviour
+{
+    public Vector3 startPos;
+    public Vector3 endPos;
+
+
+    public Vector3 rotVector;
+
+    [ContextMenu("RotateVector")]
+    void RotateVector()
+    {
+        var v = Math.RotateVector(endPos - startPos, rotVector);
+
+        Debug.Log(v);
+        Debug.DrawLine(startPos, endPos, Color.red, 1f);
+        Debug.DrawLine(startPos, startPos + v, Color.green, 1f);
+    }
+
+    [ContextMenu("ShowRotateXZAround")]
+    void ShowRotateXZAround()
+    {
+        if (!Application.isPlaying)
+        {
+            DLog.LogError("[ShowRotateXZAround] only can excute in playing");
+            return;
+        }
+
+        IEnumerator rot()
+        {
+            Debug.Log("[ShowRotateXZAround] Start");
+            Vector3 degreeV = Vector3.zero;
+
+            while (degreeV.y < 360)
+            {
+                var v = Math.RotateVector(endPos - startPos, degreeV);
+                                
+                Debug.DrawLine(startPos, startPos + v, Color.green, 1f);
+
+                degreeV.y += 2;
+                yield return new WaitForFixedUpdate();
+            }
+
+            DLog.Log("[ShowRotateXZAround] Finish");
+        }
+
+        StartCoroutine(rot());
+    }
+
+    [ContextMenu("ShowRotateXYAround")]
+    void ShowRotateXYAround()
+    {
+        if (!Application.isPlaying)
+        {
+            DLog.LogError("[ShowRotateXYAround] only can excute in playing");
+            return;
+        }
+
+        IEnumerator rot()
+        {
+            Debug.Log("[ShowRotateXYAround] Start");
+            Vector3 degreeV = Vector3.zero;
+
+            while (degreeV.z < 360)
+            {
+                var v = Math.RotateVector(endPos - startPos, degreeV);
+
+                //Debug.DrawLine(Vector3.zero, originVector, Color.red, 1f);
+                Debug.DrawLine(startPos, startPos + v, Color.green, 1f);
+
+                degreeV.z += 2;
+                yield return new WaitForFixedUpdate();
+            }
+
+            DLog.Log("[ShowRotateXYAround] Finish");
+        }
+
+        StartCoroutine(rot());
+    }
+    [ContextMenu("ShowRotateYZAround")]
+    void ShowRotateYZAround()
+    {
+        if (!Application.isPlaying)
+        {
+            DLog.LogError("[ShowRotateYZAround] only can excute in playing");
+            return;
+        }
+
+        IEnumerator rot()
+        {
+            Debug.Log("[ShowRotateYZAround] Start");
+            Vector3 degreeV = Vector3.zero;
+
+            while (degreeV.x < 360)
+            {
+                var v = Math.RotateVector(endPos - startPos, degreeV);
+
+                //Debug.DrawLine(Vector3.zero, originVector, Color.red, 1f);
+                Debug.DrawLine(startPos, startPos + v, Color.green, 1f);
+
+                degreeV.x += 2;
+                yield return new WaitForFixedUpdate();
+            }
+
+            DLog.Log("[ShowRotateYZAround] Finish");
+        }
+
+        StartCoroutine(rot());
+    }
+}
--- Assets/cardooo.core/Core/Math/Math.cs
+++ Assets/cardooo.core/Core/Math/Math.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 namespace cardooo.math
 {
+    
     public partial class Math
     {
+        /// <summary>
+        /// 向量三次元旋轉
+        /// HINT:非原點向量請記得將結果與輸入向量原點相加
+        /// </summary>
+        /// <param name="originVector">單位：向量</param>
+        /// <param name="rotationInDegrees">單位：角度</param>
+        /// <returns>單位：向量</returns>
+        public static Vector3 RotateVector(Vector3 originVector, Vector3 rotationInDegrees)
+        {
+            // 創建旋轉矩陣
+            Matrix4x4 rotationMatrix = Matrix4x4.Rotate(Quaternion.Euler(rotationInDegrees));            
+            // 將向量應用旋轉矩陣
+            Vector3 rotatedVector = rotationMatrix.MultiplyVector(originVector);
+            return rotatedVector;
+        }
+
         public static Vector2 GetPointOnCircleByAngle(Vector2 center, float radius, float angle)
         {            
             float x = center.x + radius * Mathf.Cos(angle * Mathf.PI / 180);
@@ -18,5 +35,11 @@ namespace cardooo.math
             float angle = Random.Range(0f, 360f);
             return GetPointOnCircleByAngle(center, radius, angle);
         }
+
+        //
+        //矩陣向量位移        
+        //拋物線
+        //重力模擬
+        //
     }
 }
